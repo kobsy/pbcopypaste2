31b96828316f293ae701c9ba99dd2c6e4b844cbb
Matt Kobs <matt.kobs@cph.org>
2023-11-06 14:23:21 -0600

    [skip] Made scripts compiled, standalone files (15m)
    Sonoma doesn’t link scripts correctly, so we have to go ahead and compile these into binaries and place them into the path for everything to work… 🤷‍♂️

--- pbpaste2.swift
+++ pbpaste2.swift
@@ -1,17 +1,19 @@
-#!/usr/bin/swift
-
-import Foundation
 import AppKit
 
-var pboardType = NSPasteboard.PasteboardType.string
+@main
+struct PBPaster {
+    static func main() {
+        var pboardType = NSPasteboard.PasteboardType.string
 
-if CommandLine.arguments.contains("--html") {
-  pboardType = NSPasteboard.PasteboardType.html
-}
+        if CommandLine.arguments.contains("--html") {
+            pboardType = NSPasteboard.PasteboardType.html
+        }
 
-let pboard = NSPasteboard.general
-if let output = pboard.string(forType: pboardType) {
-  print(output)
-} else {
-  print("Nothing found with that format in the pasteboard");
+        let pboard = NSPasteboard.general
+        if let output = pboard.string(forType: pboardType) {
+            print(output)
+        } else {
+            print("Nothing found with that format in the pasteboard");
+        }
+    }
 }
